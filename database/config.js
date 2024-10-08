b16b7648fc79fdf89fcaebbb92541fb2868fdd89
Daniel Ramirez <danielramirezduque1@gmail.com>
2024-10-08 14:45:09 -0500

    Add mysql2 and bcrypt this project

---
+++ database/config.js
@@ -0,0 +1,12 @@
+const mysql = require('mysql2')
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'DRD#99#drdr'
+})
+
+connection.connect(function (err) {
+  if (err) throw err
+  console.log('Connected To Database!')
+})
